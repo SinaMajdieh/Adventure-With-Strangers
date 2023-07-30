069ccf757cc950d67a48ae0dcbf3a19e2b5b07e7
SinaMajdieh <sina.majdieh@gmail.com>
2023-07-30 10:40:51 +0330

    comment on form models attributes

--- internal/models/forms.go
+++ internal/models/forms.go
@@ -4,13 +4,21 @@ package models
 
 // Register Form model
 type RegisterForm struct {
+	// Username of the user
 	Username     string `json:"username"`
+	
+	// Password the user put in
 	Password     string `json:"password"`
+	
+	// Password confirmation to make sure user didn't make a typo
 	Confirmation string `json:"confirmation"`
 }
 
 // Login Form model
 type LoginForm struct {
+	// Username of the user
 	Username string `json:"username"`
+	
+	// User's password for login validation
 	Password string `json:"password"`
 }
